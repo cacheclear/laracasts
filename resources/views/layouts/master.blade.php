97cc728221a4ff8c292a518b62af35aa7015f282
cacheclear <contact@dennisking.de>
2018-07-09 16:34:34 +0200

    structures layout in partial templates with blade

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,38 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Album example for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+<header>
+   @include('layouts.nav')
+</header>
+
+<main role="main">
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+</main>
+
+@include('layouts.footer')
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@
 
 Route::get('/posts', 'PostsController@index');
 
+Route::get('/posts/{post}', 'PostsController@show');
+
 Route::get('/tasks', 'TasksController@index');
 
 Route::get('/tasks/{task}', 'TasksController@show');
