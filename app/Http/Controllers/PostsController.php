1fa0bd36b975b644bdf876dd3b702a2e5af61c5d
cacheclear <contact@dennisking.de>
2018-07-09 18:01:49 +0200

    adds form template and store method to controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,17 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index', []);
+        return view('posts.index');
     }
 
     public function show($post)
     {
-        return view('posts.show', []);
+        return view('posts.show');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        $post = new Post;
+
+        $post->title = request('title');
+        $post->body = request('body');
+
+        $post->save();
+
+        return view('posts.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,11 @@
 
 Route::get('/posts', 'PostsController@index');
 
-Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/create', 'PostsController@create');
+
+Route::post('/posts', 'PostsController@store');
+
+//Route::get('/posts/{post}', 'PostsController@show');
 
 Route::get('/tasks', 'TasksController@index');
 
