4600ba4719d29f2c810888c56afaa6539f1d0a81
cacheclear <contact@dennisking.de>
2018-07-10 15:03:36 +0200

    moves error messages to separate template

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -23,15 +23,7 @@
         </div>
     </form>
 
-    @if(count($errors))
-        <div class="alert alert-danger">
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{$error}}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+    @include('layouts.errors')
 
 @endsection
 
