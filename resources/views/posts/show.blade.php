13c4b10df019c4fc46c492e93e2ea01ed2cbf19c
cacheclear <contact@dennisking.de>
2018-07-10 16:56:35 +0200

    renders comments in show template

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.master')
 
 @section('content')
+
     @include('posts.post')
+
+    <hr>
+
+    <div class="comments">
+        <ul class="list-group">
+            @foreach($post->comments as $comment)
+                <li class="list-group-item">
+                    <strong>{{$comment->created_at->diffForHumans()}}:&nbsp;</strong>
+                    {{$comment->body}}
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
