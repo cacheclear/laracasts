8a16dc853e5ff9daaef7116b2565c7733a6a49e2
cacheclear <contact@dennisking.de>
2018-07-10 16:45:49 +0200

    adds comment model and creates a relationship to post

--- app/Post.php
+++ app/Post.php
@@ -5,4 +5,9 @@ namespace App;
 class Post extends Model
 {
     protected $fillable = ['title', 'body'];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
