036c541cf8966afb268489d24eb70a2b49b8a736
cacheclear <contact@dennisking.de>
2018-07-11 18:02:05 +0200

    adds config for testdatabase to phpunit.xml

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Post;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -16,9 +17,17 @@ class ExampleTest extends TestCase
     {
         // Given I have records in the database that are posts,
         // and each one is posted a month apart.
+        $first = factory(Post::class)->create();
+
+        $second = factory(Post::class)->create([
+            'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
 
         // When I fetch the archives.
+        $posts = Post::archives();
+
 
         // Then the response should be in the right format.
+        $this->assertCount(2, $posts);
     }
 }
