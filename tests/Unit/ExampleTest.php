5dff02ee71be464ee20a08ff73979b3ecfd49b9f
cacheclear <contact@dennisking.de>
2018-07-11 17:58:07 +0200

    adds given-when-then-plan to unittest method

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -14,6 +14,11 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        // Given I have records in the database that are posts,
+        // and each one is posted a month apart.
+
+        // When I fetch the archives.
+
+        // Then the response should be in the right format.
     }
 }
