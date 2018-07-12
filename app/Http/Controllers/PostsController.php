84fde47a196ab1f82f8ad4b4fd083071a237aaf2
cacheclear <contact@dennisking.de>
2018-07-12 20:50:55 +0200

    adds flash message for publishing post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -45,6 +45,8 @@ class PostsController extends Controller
             new Post(request(['title', 'body']))
         );
 
+        session()->flash('message', 'Your post has been published!');
+
         return redirect()->home();
     }
 }
