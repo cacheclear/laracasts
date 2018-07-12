4e1621e11cb112dca60de806c05a73b3f6845179
cacheclear <contact@dennisking.de>
2018-07-12 22:35:53 +0200

    adds simple event example

--- app/Listeners/NotifySubscribers.php
+++ app/Listeners/NotifySubscribers.php
@@ -26,6 +26,6 @@ class NotifySubscribers
      */
     public function handle(ThreadCreated $event)
     {
-        //
+        var_dump($event->thread['name'] . ' was published to the forum.');
     }
 }
